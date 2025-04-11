c271c3f824864443f47c0db342c67bd7f87640e3
Thomas Bany <mun.bany@gmail.com>
2025-04-11 21:04:16 +0200

    Build script can now be run from any wd

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-podman build . -t docker.io/meuna/lsdc2:valheim --format docker \
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+podman build $script_dir -t docker.io/meuna/lsdc2:valheim --format docker \
 && podman push docker.io/meuna/lsdc2:valheim
